167715a14a98a1082a557dee9227a1cdf6009706
jarodriguezp <jesus.jarp@gmail.com>
2023-09-16 09:46:18 +0200

    adding next and integration with laravel API

---
+++ spa/src/app/page.tsx
@@ -0,0 +1,24 @@
+"use client";
+import styles from "./page.module.css";
+import { useEffect, useState } from "react";
+
+export default function Home() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("http://127.0.0.1:8000/api/products");
+        const result = await response.json();
+        console.log(result);
+        setData(result);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return <div>Aplicación</div>;
+}
